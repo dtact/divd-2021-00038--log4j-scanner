31c2a1628897cc772f7751fc7219559874e0408b
Remco Verhoef <remco@dutchcoders.io>
2021-12-18 19:11:17 +0100

    update assigned score

--- app/app.go
+++ app/app.go
@@ -161,7 +161,8 @@ var CVE_2021_44228 = CVE{
 	Description: `Apache Log4j2 2.0-beta9 through 2.12.1 and 2.13.0 through 2.15.0 JNDI features used in configuration, log messages, and parameters do not protect against attacker controlled LDAP and other JNDI related endpoints. An attacker who can control log messages or log message parameters can execute arbitrary code loaded from LDAP servers when message lookup substitution is enabled. From log4j 2.15.0, this behavior has been disabled by default. From version 2.16.0, this functionality has been completely removed. Note that this vulnerability is specific to log4j-core and does not affect log4net, log4cxx, or other Apache Logging Services projects. `,
 	Reference: []string{
 		"https://logging.apache.org/log4j/2.x/security.html",
-		"https://www.cve.org/CVERecord?id=CVE-2021-4428",
+		"https://www.cve.org/CVERecord?id=CVE-2021-44228",
+		"https://logging.apache.org/log4j/2.x/security.html#CVE-2021-44228",
 	},
 	Score: 9.3,
 }
@@ -172,6 +173,7 @@ var CVE_2021_45046 = CVE{
 	Reference: []string{
 		"https://logging.apache.org/log4j/2.x/security.html",
 		"https://www.cve.org/CVERecord?id=CVE-2021-45046",
+		"https://logging.apache.org/log4j/2.x/security.html#CVE-2021-45046",
 	},
 	Score: 2.6,
 }
@@ -182,9 +184,9 @@ var CVE_2021_45105 = CVE{
 	Reference: []string{
 		"https://logging.apache.org/log4j/2.x/security.html",
 		"https://www.cve.org/CVERecord?id=CVE-2021-45105",
+		"https://logging.apache.org/log4j/2.x/security.html#CVE-2021-45105",
 	},
-	// TODO: NO SCORE ASSIGNED YET
-	Score: 6.0,
+	Score: 7.5,
 }
 
 var vulnerabilities = map[string][]CVE{
